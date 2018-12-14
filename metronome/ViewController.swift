01e57367165fe39d5e36d23fae155ce054d1d9a9
Brandon Li <brandon.li820@icloud.com>
2018-12-14 15:58:26 -0700

    first commit

---
+++ metronome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  metronome
+//
+//  Created by Brandon Li on 12/14/18.
+//  Copyright © 2018 Brandon Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
