b2e39a410db9138c14218c5e8d6d3709d318a9d4
Brandon Li <brandon.li820@icloud.com>
2018-12-14 23:35:21 -0700

    slider ui

---
+++ metronome/Metronome.swift
@@ -0,0 +1,16 @@
+//
+//  Metronome.swift
+//  metronome
+//
+//  Created by Brandon Li on 12/14/18.
+//  Copyright © 2018 Brandon Li. All rights reserved.
+//
+
+import Foundation
+class Metronome {
+    
+    var bpm = 40
+    
+    
+    
+}
--- metronome/ViewController.swift
+++ metronome/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var bpmLabel: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!
+    var metronome = Metronome()
+    var bpm:Int {
+        get {
+            return Int(slider.value)
+        }
+        set{
+            slider.value = Float(newValue)
+            bpmLabel.text = "\(bpm)"
+            metronome.bpm = bpm
+        }
     }
-
-
+    @IBAction func changeBpm(_ sender: UISlider) {
+        bpmLabel.text = "\(bpm)"
+    }
+    @IBAction func addBpm(_ sender: UIButton) {
+        bpm += 1
+    }
+    
+    @IBAction func subtractBpm(_ sender: UIButton) {
+        bpm -= 1
+    }
 }
 
