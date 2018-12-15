d1661a6a574639060d58775bc952100cc0fbf2b8
Brandon Li <brandon.li820@icloud.com>
2018-12-15 12:38:45 -0700

    added metronome functionality

--- metronome/ViewController.swift
+++ metronome/ViewController.swift
@@ -7,32 +7,95 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var audioPlayer = AVAudioPlayer()
+    var timer = Timer()
+    var state = "pause" {
+        didSet {
+            if state == "play" {
+                playButton.setBackgroundImage(UIImage(named: "pauseButton.png"), for: UIControl.State.normal)
+                timer.invalidate()
+                timer = Timer.scheduledTimer(timeInterval: (pow((Double(bpm)/60.0), -1.0)), target: self, selector: #selector(playAudio), userInfo: nil, repeats: true)
+            }
+            else {
+                playButton.setBackgroundImage(UIImage(named: "playButton.png"), for: UIControl.State.normal)
+                timer.invalidate()
 
+    
+            }
+           
+        }
+    }
     @IBOutlet weak var bpmLabel: UILabel!
     
+    @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var slider: UISlider!
-    var metronome = Metronome()
     var bpm:Int {
         get {
+
             return Int(slider.value)
         }
         set{
             slider.value = Float(newValue)
             bpmLabel.text = "\(bpm)"
-            metronome.bpm = bpm
+
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let sound = Bundle.main.path(forResource: "metronomeBeat", ofType: "mp3")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        }
+        catch {
+            print("error")
         }
     }
     @IBAction func changeBpm(_ sender: UISlider) {
+        if state == "play" {
+            state = "pause"
+            state = "play"
+        }
+      
         bpmLabel.text = "\(bpm)"
+      
     }
     @IBAction func addBpm(_ sender: UIButton) {
-        bpm += 1
+        if state == "play" {
+            state = "pause"
+            bpm += 1
+            state = "play"
+        }
+        else {
+            bpm += 1
+        }
     }
     
     @IBAction func subtractBpm(_ sender: UIButton) {
-        bpm -= 1
+        if state == "play" {
+            state = "pause"
+            bpm -= 1
+            state = "play"
+        }
+        else {
+            bpm -= 1
+        }
+    }
+    @IBAction func play(_ sender: UIButton) {
+        if state == "pause" {
+            state = "play"
+        }
+        else {
+            state = "pause"
+        }
+
+    }
+    @objc func playAudio(){
+        audioPlayer.play()
     }
 }
 
